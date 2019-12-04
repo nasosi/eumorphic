07ee5f3f58520caca3e4f40e74d2d77c0beb7b13
Nasos <niliopoulos@pointsemantics.com>
2019-12-03 20:50:49 -0500

    Added a function so that the collection types can be extended. This results in a new collection type.

--- example/07_add_types/main.cpp
+++ example/07_add_types/main.cpp
@@ -5,16 +5,14 @@
 
 struct a {};  
 void print(const a&) { std::cout << "a\n"; }
-void print2(const a&) {std::cout << "a2\n";}
 
 struct b {};  
 void print(const b&) { std::cout << "b\n"; }
 
 struct c {};  
 void print(const c&) { std::cout << "c\n"; } 
-void print2(const c&) { std::cout << "c2\n"; }
 
-void print(int i) { std::cout << i << "\n"; }
+void print(int i) { std::cout << "Int: " << i << "\n"; }
 
 template <class T> using container = std::vector<T>;
 using heap_collection = eumorphic::collection< container, a, b, c >;
@@ -31,15 +29,19 @@ int main()
 	collection.insert(b{});
 	collection.insert(b{});
 
-	std::cout << "Execute over all types for which the expression is valid\n";
+	std::cout << "Original collection\n";
 	eumorphic::for_each(collection, [](auto&& v)
 		{
-			auto can_apply = boost::hana::is_valid([](auto&& p) -> decltype(print2(p)) {});
-
-			if constexpr (decltype(can_apply(v))::value)
-			{
-				print2(v);
-			}
+			print(v);
 		});
 
+	auto extended_collection = collection.add_types_and_copy<int>();
+	extended_collection.insert(2);
+	extended_collection.insert(3);
+
+	std::cout << "Extended collection\n";
+	eumorphic::for_each(extended_collection, [](auto&& v)
+		{
+			print(v);
+		});
 }
